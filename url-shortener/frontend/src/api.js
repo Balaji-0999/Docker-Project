845871780500ddf6d32279df53686d59e5ecee26
Ubuntu <ubuntu@ip-172-31-29-229.ec2.internal>
2026-09-03 12:04:04 +0000

    feat: update API integration

--- url-shortener/frontend/src/api.js
+++ url-shortener/frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://54.92.201.3:5000/api';
+const API_BASE = 'http://3.94.162.222:5000/api';
 
 export const REDIRECT_BASE = API_BASE.replace('/api', '');
 
@@ -34,8 +34,8 @@ export const registerUser = (name, email, password) =>
 export const loginUser = (email, password) =>
   api.post('/auth/login', { email, password });
 
-export const shortenUrl = (originalUrl, customAlias) =>
-  api.post('/links/shorten', { originalUrl, customAlias });
+export const shortenUrl = (originalUrl, customAlias, expiresIn, password) =>
+  api.post('/links/shorten', { originalUrl, customAlias, expiresIn, password });
 
 export const getLinks = () => api.get('/links');
 
